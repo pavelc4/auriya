8eca923ecd64b478bacaaa46b9fd172300c2e4ba
pavelc4 <dimzzy23@gmail.com>
2025-12-06 04:31:56 +0700

    refactor: Remove legacy FAS code
    Signed-off-by: pavelc4 <dimzzy23@gmail.com>

--- src/core/scaling/mod.rs
+++ src/core/scaling/mod.rs
@@ -4,20 +4,3 @@ pub enum ScalingAction {
     Maintain,
     Reduce,
 }
-
-pub fn decide_from_gpu_util(gpu_util: f32, margin: f32, thermal_throttle: bool) -> ScalingAction {
-    if thermal_throttle {
-        return ScalingAction::Reduce;
-    }
-
-    let boost_threshold = 85.0 - margin;
-    let reduce_threshold = 40.0 + margin;
-
-    if gpu_util > boost_threshold {
-        ScalingAction::Boost
-    } else if gpu_util < reduce_threshold {
-        ScalingAction::Reduce
-    } else {
-        ScalingAction::Maintain
-    }
-}
