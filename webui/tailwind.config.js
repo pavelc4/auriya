cd9a59226e92b1e0aedbacf8de61f7f9cbddd725
pavelc4 <dimzzy23@gmail.com>
2025-12-06 05:06:08 +0700

    style: Remove unnecessary comments from Tailwind CSS configuration.

--- webui/tailwind.config.js
+++ webui/tailwind.config.js
@@ -6,7 +6,6 @@ export default {
     ],
     theme: {
         extend: {
-            // Kita tetapkan warna di sini juga untuk utility class Tailwind biasa
             colors: {
                 'surface': '#1a110d',
                 'on-surface': '#f0ded8',
@@ -22,40 +21,35 @@ export default {
     plugins: [
         require('daisyui'),
     ],
-    // INI KUNCINYA: Definisi Tema DaisyUI
     daisyui: {
         themes: [
             {
                 auriya: {
-                    "primary": "#b7410e",          /* Rust Orange (Gantiin Ungu) */
+                    "primary": "#b7410e",
                     "primary-content": "#481616ff",
 
-                    "secondary": "#77574e",        /* Brownish */
+                    "secondary": "#77574e",
                     "secondary-content": "#ffffff",
 
-                    "accent": "#6b5d2e",           /* Olive */
+                    "accent": "#6b5d2e",
                     "accent-content": "#ffffff",
 
                     "neutral": "#1a110d",
                     "neutral-content": "#f0ded8",
 
-                    "base-100": "#171209",         /* Background Utama */
-                    "base-200": "#251a16",         /* Card Background */
+                    "base-100": "#171209",
+                    "base-200": "#251a16",
                     "base-300": "#3b2e2a",
-                    "base-content": "#ece0d1",     /* Text Color */
-
+                    "base-content": "#ece0d1",
                     "info": "#00add8",
                     "success": "#219138",
                     "warning": "#f7df1e",
                     "error": "#ba1a1a",
-
-                    // Setup radius agar sesuai desain kamu
                     "--rounded-btn": "9999px",
                     "--rounded-box": "1.5rem",
                 },
             },
         ],
-        // Paksa gunakan tema ini
         darkTheme: "auriya",
     },
 }
