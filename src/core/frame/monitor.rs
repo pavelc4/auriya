bd8fb5fef8f5fab04d6b49ecd146edeeb01e5b9d
pavelc4 <dimzzy23@gmail.com>
2025-12-06 04:31:19 +0700

    feat: Add FrameMonitor
    Signed-off-by: pavelc4 <dimzzy23@gmail.com>

---
+++ src/core/frame/monitor.rs
@@ -0,0 +1,45 @@
+use crate::core::dumpsys::surfaceflinger::SurfaceFlinger;
+use anyhow::Result;
+
+pub struct FrameMonitor {
+    pub package: String,
+    layer_name: Option<String>,
+}
+
+impl FrameMonitor {
+    pub fn new(package: String) -> Self {
+        Self {
+            package,
+            layer_name: None,
+        }
+    }
+
+    pub fn get_frame_time(&mut self) -> Result<f32> {
+        if self.layer_name.is_none() {
+            if let Ok(Some(layer)) = SurfaceFlinger::find_layer(&self.package) {
+                tracing::info!(target: "auriya::fas", "Found SurfaceFlinger layer: {}", layer);
+                self.layer_name = Some(layer);
+            }
+        }
+
+        if let Some(layer) = &self.layer_name {
+            match SurfaceFlinger::get_frame_time(layer) {
+                Ok(ft) => Ok(ft),
+                Err(e) => {
+                    tracing::warn!(target: "auriya::fas", "Failed to get frame time, resetting layer: {:?}", e);
+                    self.layer_name = None;
+                    Ok(0.0)
+                }
+            }
+        } else {
+            Ok(0.0)
+        }
+    }
+
+    pub fn update_package(&mut self, package: String) {
+        if self.package != package {
+            self.package = package;
+            self.layer_name = None;
+        }
+    }
+}
