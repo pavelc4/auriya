dbafdafdd8197988b9e859028c8876019c8a008d
pavelc4 <dimzzy23@gmail.com>
2025-12-07 18:43:11 +0700

    chore(webui): clean up vite build artifacts and adjust config
    Signed-off-by: pavelc4 <dimzzy23@gmail.com>

--- webui/vite.config.js
+++ webui/vite.config.js
@@ -3,18 +3,17 @@ import tailwindcss from '@tailwindcss/vite'
 import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        viteSingleFile(),
-    ],
-    build: {
-        target: 'esnext',
-        assetsInlineLimit: 100000000,
-        chunkSizeWarningLimit: 100000000,
-        cssCodeSplit: false,
-        brotliSize: false,
-        rollupOptions: {
-            inlineDynamicImports: true,
-        },
-    }
+	plugins: [
+		tailwindcss(),
+		viteSingleFile(),
+	],
+	build: {
+		target: 'esnext',
+		assetsInlineLimit: 100000000,
+		chunkSizeWarningLimit: 100000000,
+		cssCodeSplit: false,
+		brotliSize: false,
+		rollupOptions: {
+		},
+	}
 })
