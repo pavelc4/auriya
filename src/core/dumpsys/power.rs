7a2ac5c8e4170eed425ba88412f9d9191cffa425
pavelc4 <dimzzy23@gmail.com>
2025-12-14 19:46:00 +0700

    refactor(power): use direct struct initialization and remove unused fields
    Signed-off-by: pavelc4 <dimzzy23@gmail.com>

--- src/core/dumpsys/power.rs
+++ src/core/dumpsys/power.rs
@@ -3,9 +3,7 @@ use crate::core::cmd::run_cmd_timeout_async;
 #[derive(Debug, Default, Clone)]
 pub struct PowerState {
     pub screen_awake: bool,
-    pub is_plugged_in: bool,
     pub battery_saver: bool,
-    pub battery_saver_sticky: bool,
 }
 
 impl PowerState {
@@ -24,25 +22,18 @@ impl PowerState {
         };
 
         let s = String::from_utf8_lossy(&out.stdout);
-        let mut ps = PowerState::default();
 
-        ps.battery_saver = battery_saver_fast.unwrap_or_else(|| {
-            s.contains("mBatterySaverEnabled=true")
-                || s.contains("mSettingBatterySaverEnabled=true")
-                || s.contains("Battery Saver: ON")
-        });
-
-        ps.screen_awake = s.contains("mWakefulness=Awake")
-            || s.contains("mAwake=true")
-            || s.contains("mInteractive=true")
-            || s.contains("mScreenOn=true");
-
-        ps.battery_saver_sticky = s.contains("mBatterySaverSticky=true")
-            || s.contains("mSettingBatterySaverEnabledSticky=true");
-
-        ps.is_plugged_in = s.contains("mIsPowered=true")
-            || s.contains("plugged=true")
-            || (s.contains("Plug Type:") && !s.contains("Plug Type: NONE"));
+        let ps = PowerState {
+            battery_saver: battery_saver_fast.unwrap_or_else(|| {
+                s.contains("mBatterySaverEnabled=true")
+                    || s.contains("mSettingBatterySaverEnabled=true")
+                    || s.contains("Battery Saver: ON")
+            }),
+            screen_awake: s.contains("mWakefulness=Awake")
+                || s.contains("mAwake=true")
+                || s.contains("mInteractive=true")
+                || s.contains("mScreenOn=true"),
+        };
 
         Ok(ps)
     }
