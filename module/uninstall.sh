9ce8c4bb022f38bda019c8e74afcf1f2efe93ec8
pavelc4 <pavelc4@users.noreply.github.com>
2026-02-07 23:05:30 +0700

    fix(module): correct uninstall script binary name and cleanup
    Signed-off-by: pavelc4 <pavelc4@users.noreply.github.com>

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,5 +1,9 @@
 #!/system/bin/sh
 
+# Stop Daemon
+killall -9 auriya 2>/dev/null
+rm -f /dev/socket/auriya.sock
+
 # Config and Logs
 rm -rf /data/adb/.config/auriya
 rm -rf /data/adb/auriya
@@ -7,8 +11,8 @@ rm -rf /data/adb/auriya
 # Symlinks (KSU/APatch)
 rm -f /data/adb/ksu/bin/auriya
 rm -f /data/adb/ap/bin/auriya
-rm -f /data/adb/ksu/bin/auriya-ctl
-rm -f /data/adb/ap/bin/auriya-ctl
+rm -f /data/adb/ksu/bin/auriyactl
+rm -f /data/adb/ap/bin/auriyactl
 
 # Post-fs-data/Service scripts (if any external ones were created, though typically handled by module system)
 # We only clean our custom directories and symlinks.
